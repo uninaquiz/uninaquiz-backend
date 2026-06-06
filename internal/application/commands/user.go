552b74fe1abde4e86aec0a2dcb2337d413c5882c
EmanuelErnesto <emanuel.earego@gmail.com>
2026-06-06 19:52:03 -0300

    feat(commands): create command DTOs for write operations

---
+++ internal/application/commands/user.go
@@ -0,0 +1,6 @@
+package commands
+
+type CreateUserCommand struct {
+	Username string `json:"username" binding:"required,min=3,max=50"`
+	Password string `json:"password" binding:"required,min=8,max=50"`
+}
